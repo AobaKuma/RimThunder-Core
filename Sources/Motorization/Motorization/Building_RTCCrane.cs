ac2ce107fc22d80a0751e08f4f40d9e4f7b35e2e
CMDR-Bill-Doors <Billyanghk@qq.com>
2023-10-13 15:14:17 +0100

    crane top render

--- Sources/Motorization/Motorization/Building_RTCCrane.cs
+++ Sources/Motorization/Motorization/Building_RTCCrane.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using RimWorld;
@@ -15,16 +16,35 @@ namespace Motorization
     {
         public Bill_RTCVehicle CurrentBill;
 
+        public float craneTargetPct;
+
+        public float craneCurrentPct;
+
+        public float cranePauseTicks;
+
+        ModExtension_CraneTopData craneTopData => def.GetModExtension<ModExtension_CraneTopData>();
+
+        System.Random rand;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            rand = new System.Random();
+        }
+
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_References.Look(ref CurrentBill, "CurrentBill");
+            Scribe_Values.Look(ref craneCurrentPct, "craneCurrentPct");
+            Scribe_Values.Look(ref craneTargetPct, "craneTargetPct");
         }
 
         public override void Draw()
         {
             base.Draw();
             CurrentBill?.ResultGraphic?.Draw(DrawPos + new Vector3(0, -0.01f, -0.2f), Rotation, this);
+            craneTopData?.graphicData.Draw(DrawPos + craneTopData.GetDrawOffset(Rotation, craneCurrentPct), Rotation, this);
         }
 
         public bool CellOccupied()
@@ -42,6 +62,41 @@ namespace Motorization
                 }
             }
             return false;
+        }
+
+        public override void UsedThisTick()
+        {
+            base.UsedThisTick();
+            CraneTick();
+        }
+
+        public void CraneTick()
+        {
+            if (craneTopData == null)
+            {
+                return;
+            }
+            if (craneCurrentPct == craneTargetPct)
+            {
+                craneTargetPct = (float)rand.NextDouble();
+                cranePauseTicks = craneTopData.pauseTickRange.RandomInRange;
+            }
+            if (cranePauseTicks <= 0)
+            {
+                float speed = craneTopData.PctPerSec / 60;
+                if (Math.Abs(craneTargetPct - craneCurrentPct) <= speed)
+                {
+                    craneCurrentPct = craneTargetPct;
+                }
+                else
+                {
+                    _ = (craneTargetPct < craneCurrentPct) ? craneCurrentPct -= speed : craneCurrentPct += speed;
+                }
+            }
+            else
+            {
+                cranePauseTicks--;
+            }
         }
 
         public override void Notify_BillDeleted(Bill bill)
@@ -63,5 +118,69 @@ namespace Motorization
                 CurrentBill = null;
             }
         }
+    }
+
+    internal class ModExtension_CraneTopData : DefModExtension
+    {
+        public Graphic graphicData;
+
+        public CraneEndpoints EndpointSouth;
+
+        public CraneEndpoints EndpointNorth;
+
+        public CraneEndpoints EndpointEast;
+
+        public CraneEndpoints EndpointWest;
+
+        public float PctPerSec = 5f;
+
+        public IntRange pauseTickRange = new IntRange(60, 600);
+
+        public Vector3 GetDrawOffset(Rot4 rotation, float pct)
+        {
+            if (EndpointNorth == null || EndpointEast == null)
+            {
+                return Vector3.zero;
+            }
+
+            if (EndpointSouth == null)
+            {
+                EndpointSouth = EndpointNorth;
+            }
+
+            if (EndpointWest == null)
+            {
+                EndpointWest = new CraneEndpoints()
+                {
+                    endPointA = new Vector3(-EndpointEast.endPointA.x, EndpointEast.endPointA.y, EndpointEast.endPointA.z),
+                    endPointB = new Vector3(-EndpointEast.endPointB.x, EndpointEast.endPointB.y, EndpointEast.endPointB.z),
+                };
+            }
+
+            switch (rotation.AsInt)
+            {
+                case 0:
+                    return DrawOffsetPct(EndpointNorth.endPointA, EndpointNorth.endPointB, pct);
+                case 1:
+                    return DrawOffsetPct(EndpointEast.endPointA, EndpointEast.endPointB, pct);
+                case 2:
+                    return DrawOffsetPct(EndpointSouth.endPointA, EndpointSouth.endPointB, pct);
+                case 3:
+                    return DrawOffsetPct(EndpointWest.endPointA, EndpointWest.endPointB, pct);
+            }
+            return Vector3.zero;
+        }
+
+        Vector3 DrawOffsetPct(Vector3 A, Vector3 B, float pct)
+        {
+            return A + (A - B) * pct;
+        }
+    }
+
+    internal class CraneEndpoints
+    {
+        public Vector3 endPointA;
+
+        public Vector3 endPointB;
     }
 }
