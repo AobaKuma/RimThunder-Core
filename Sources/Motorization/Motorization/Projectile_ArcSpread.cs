821fd823b507db86ded424674d7b020db7cc975d
CMDR-Bill-Doors <Billyanghk@qq.com>
2023-11-06 10:17:33 +0000

    New Smoke

--- Sources/Motorization/Motorization/Projectile_ArcSpread.cs
+++ Sources/Motorization/Motorization/Projectile_ArcSpread.cs
@@ -14,22 +14,39 @@ namespace Motorization
         RotationalArcSpreadExtension Extension => def.GetModExtension<RotationalArcSpreadExtension>();
         public override void Launch(Thing launcher, Vector3 origin, LocalTargetInfo usedTarget, LocalTargetInfo intendedTarget, ProjectileHitFlags hitFlags, bool preventFriendlyFire = false, Thing equipment = null, ThingDef targetCoverDef = null)
         {
-            int num = GenRadial.NumCellsInRadius(Extension.range.max);
-            List<IntVec3> affectedCells = new List<IntVec3>();
-            for (int i = 1; i < num; i++)
+            if (Extension.random)
             {
-                IntVec3 intVec = launcher.Position + GenRadial.RadialPattern[i];
-                Vector3 vec3 = (Extension.invert ? launcher.Position - intVec : intVec - launcher.Position).ToVector3();
-                vec3.y = 0;
-                if (Math.Abs(Vector3.Angle(intendedTarget.Cell.ToVector3() - origin, vec3)) < Extension.arc && Vector3.Distance(origin, vec3) > Extension.range.min)
+                int num = GenRadial.NumCellsInRadius(Extension.range.max);
+                List<IntVec3> affectedCells = new List<IntVec3>();
+                for (int i = 1; i < num; i++)
                 {
-                    affectedCells.Add(intVec);
+                    IntVec3 intVec = launcher.Position + GenRadial.RadialPattern[i];
+                    Vector3 vec3 = (Extension.invert ? launcher.Position - intVec : intVec - launcher.Position).ToVector3();
+                    vec3.y = 0;
+                    if (Math.Abs(Vector3.Angle(intendedTarget.Cell.ToVector3() - origin, vec3)) < Extension.arc && Vector3.Distance(origin, vec3) > Extension.range.min)
+                    {
+                        affectedCells.Add(intVec);
+                    }
+                }
+                for (int i = 0; i < Extension.amount; i++)
+                {
+                    Projectile thing = (Projectile)GenSpawn.Spawn(Extension.projectile, launcher.Position, Map);
+                    thing.Launch(launcher, origin, affectedCells.RandomElement(), intendedTarget, hitFlags, preventFriendlyFire, equipment, targetCoverDef);
                 }
             }
-            for (int i = 0; i < Extension.amount; i++)
+            else
             {
-                Projectile thing = (Projectile)GenSpawn.Spawn(Extension.projectile, launcher.Position, Map);
-                thing.Launch(launcher, origin, affectedCells.RandomElement(), intendedTarget, hitFlags, preventFriendlyFire, equipment, targetCoverDef);
+                Vector3 vector = (intendedTarget.Cell.ToVector3() - origin);
+                vector.Normalize();
+                vector = vector.RotatedBy(-Extension.arc);
+                for (int i = 0; i < Extension.amount; i++)
+                {
+                    Log.Message(vector.AngleFlat().ToString());
+                    Projectile thing = (Projectile)GenSpawn.Spawn(Extension.projectile, launcher.Position, Map);
+                    thing.Launch(launcher, origin, (origin + vector * Extension.range.RandomInRange).ToIntVec3(), intendedTarget, hitFlags, preventFriendlyFire, equipment, targetCoverDef);
+
+                    vector = vector.RotatedBy(Extension.arc / (Extension.amount - 1) * 2);
+                }
             }
             Destroy();
         }
