6681394544e748d1748b8c36433d7a3e6334d704
CMDR-Bill-Doors <Billyanghk@qq.com>
2023-11-16 14:04:58 +0000

    turret restriction undraft

---
+++ Sources/Motorization/Motorization/TurretRestrictions_Undraft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Vehicles;
+
+namespace Motorization
+{
+    internal class TurretRestrictions_Undraft : TurretRestrictions
+    {
+        public override bool Disabled => !vehicle.Drafted;
+
+        public override string DisableReason => "RTC_TurretRestrictUndraftReason".Translate();
+    }
+}
