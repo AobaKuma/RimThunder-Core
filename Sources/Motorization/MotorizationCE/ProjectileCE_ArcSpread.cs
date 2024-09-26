cdb0b7ff7be1f6513e2997dce9c59c8369406e06
CMDR-Bill-Doors <Billyanghk@qq.com>
2024-09-26 05:57:55 +0100

    CE update bruh

--- Sources/Motorization/MotorizationCE/ProjectileCE_ArcSpread.cs
+++ Sources/Motorization/MotorizationCE/ProjectileCE_ArcSpread.cs
@@ -14,18 +14,18 @@ namespace MotorizationCE
     {
         RotationalArcSpreadExtension Extension => def.GetModExtension<RotationalArcSpreadExtension>();
 
-        public override void Launch(Thing launcher, Vector2 origin, float shotAngle, float shotRotation, float shotHeight = 0, float shotSpeed = -1, Thing equipment = null, float distance = -1)
+        public override void Launch(Thing launcher, Vector2 origin, float shotAngle, float shotRotation, float shotHeight = 0, float shotSpeed = -1, Thing equipment = null, float distance = -1, int TTPS = 3)
         {
             for (int i = 0; i < Extension.amount; i++)
             {
                 ProjectileCE thing = (ProjectileCE)GenSpawn.Spawn(Extension.projectile, launcher.Position, Map);
                 if (Extension.random)
                 {
-                    thing.Launch(launcher, origin, shotAngle, shotRotation + Rand.Sign + Rand.Value * Extension.arc, shotHeight, shotSpeed, equipment, distance);
+                    thing.Launch(launcher, origin, shotAngle, shotRotation + Rand.Sign + Rand.Value * Extension.arc, shotHeight, shotSpeed, equipment, distance, TTPS);
                 }
                 else
                 {
-                    thing.Launch(launcher, origin, shotAngle, (shotRotation - Extension.arc) + Extension.arc / (Extension.amount - 1) * i * 2, shotHeight, shotSpeed, equipment, distance);
+                    thing.Launch(launcher, origin, shotAngle, (shotRotation - Extension.arc) + Extension.arc / (Extension.amount - 1) * i * 2, shotHeight, shotSpeed, equipment, distance, TTPS);
                 }
             }
             Destroy();
